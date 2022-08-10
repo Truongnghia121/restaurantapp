70846ac4e5a0969dbb35e4c222073001096d598c
gianghia <truongnghia1103@gmail.com>
2022-08-10 18:37:35 +0700

    HomeContainer 3 & crate data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,7 +43,7 @@ const Header = () => {
     });
   };
   return (
-    <div className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16">
+    <header className="fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary">
       {/* Desktop & tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to={"/"} className="flex items-center gap-2">
@@ -180,7 +180,7 @@ const Header = () => {
           )}
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Delivery from "../img/delivery.png";
 import HeroBg from "../img/heroBg.png";
+import { heroData } from "../utils/data";
 
 const HomeContainer = () => {
   return (
@@ -47,14 +48,43 @@ const HomeContainer = () => {
           Order Now
         </button>
       </div>
-      <div className="py-2 flex-1 flex">
+      <div className="py-2 flex-1 flex items-center relative">
         <img
           src={HeroBg}
           className="ml-auto h-420 w-full lg:w-auto  lg:h-650"
           alt="hero-bg"
         />
 
-        <div className="w-full h-full absolute flex items-center justify-center"></div>
+        <div
+          className="w-full h-full absolute top-0 left-0 
+         flex items-center justify-center py-4 gap-4 lg:px-32 flex-wrap drop-shadow-lg"
+        >
+          {/* heroData dùng vòng lập lấy dữ liệu */}
+          {heroData &&
+            heroData.map((n) => (
+              <div
+                key={n.id}
+                className="w-190 lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex 
+                items-center justify-center flex-col"
+              >
+                <img
+                  src={n.imageSrc}
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20  "
+                  alt="I1"
+                />
+                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {n.name}
+                </p>
+                <p className="text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {n.decp}
+                </p>
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-sx text-red-600">$</span>
+                  {n.price}
+                </p>
+              </div>
+            ))}
+        </div>
       </div>
     </section>
   );
