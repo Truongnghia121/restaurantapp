7d108a94a1970aa663e54ba5ecc4fa3926475ac0
gianghia <truongnghia1103@gmail.com>
2022-08-10 14:58:45 +0700

    HomeContainer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -117,10 +117,18 @@ const Header = () => {
       </div>
       {/* Mobile */}
       <div className="flex md:hidden w-full h-full items-center justify-between">
+        <div className="relative flex items-center justify-center">
+          <MdShoppingBasket className="text-textColor text-2xl cursor-pointer" />
+          <div className="absolute -top-0.5 -right-2 w-4 h-4 rounded-full bg-cartNumBg flex items-center justify-center">
+            <p className="text-xs text-white font-semibold">2</p>
+          </div>
+        </div>
+
         <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
           <p className="text-headingColor text-xl font-bold">City</p>
         </Link>
+
         <div className="relative">
           <motion.img
             whileTap={{ scale: 0.6 }}
