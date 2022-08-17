b4edb351ba83f9269a8543a16ea6651be32072a6
gianghia <truongnghia1103@gmail.com>
2022-08-18 00:18:42 +0700

    Filtering Menu

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useRef } from "react";
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
+import NotFound from "../img/NotFound.svg";
 
 const RowContainer = ({ flag, data, scrollValue }) => {
   console.log(data);
@@ -12,25 +14,28 @@ const RowContainer = ({ flag, data, scrollValue }) => {
     <div
       ref={rowContainer}
       className={`w-full flex items-center gap-3 my-12 scroll-smooth ${
-        flag
-          ? "overflow-x-scroll scrollbar-none"
-          : "overflow-x-hidden flex-wrap"
+        flag ? "overflow-x-scroll scrollbar-none" : "flex-wrap justify-center"
       }`}
     >
-      {data &&
+      {data.length > 0 ? (
         data.map((item) => (
           <div
             key={item?.id}
             className=" w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px] bg-cardOverlay rounded-lg p-2 my-12 shadow-md 
       backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative"
           >
-            <div className="w-full flex items-center justify-between h-[64px]">
-              <motion.img
-                whileHover={{ scale: 1.2 }}
-                src={item?.imageUrl}
-                alt=""
+            <div className="w-full flex items-center justify-between ">
+              <motion.div
                 className="w-40 -mt-8 drop-shadow-2xl"
-              />
+                whileHover={{ scale: 1.2 }}
+              >
+                <img
+                  src={item?.imageUrl}
+                  alt=""
+                  className="w-full h-full object-contain"
+                />
+              </motion.div>
+
               <motion.div
                 whileTap={{ scale: 0.75 }}
                 className="w-8 h-8 rounded-full bg-red-600 flex items-center 
@@ -54,7 +59,15 @@ const RowContainer = ({ flag, data, scrollValue }) => {
               </div>
             </div>
           </div>
-        ))}
+        ))
+      ) : (
+        <div className="w-full flex flex-col items-center justify-center">
+          <img src={NotFound} className="h-340" />
+          <p className="text-xl text-headingColor font-semibold my-2">
+            Items Not Available
+          </p>
+        </div>
+      )}
     </div>
   );
 };
