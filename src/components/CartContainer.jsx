731fdfe567d11c5ee597abb3ddebf91bb63122c3
gianghia <truongnghia1103@gmail.com>
2022-08-19 18:11:13 +0700

    Updating Cart Total & Quantity

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
 import { motion } from "framer-motion";
 import { RiRefreshFill } from "react-icons/ri";
-import { BiMinus, BiPlus } from "react-icons/bi";
 import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
 import EmptyCart from "../img/emptyCart.svg";
+import CartItem from "./CartItem";
 
 const CartContainer = () => {
   const [{ cartShow, cartItems, user }, dispatch] = useStateValue();
+  const [flag, setFlag] = useState(1);
+  const [tot, setTot] = useState(0);
   // Cart
   const showCart = () => {
     dispatch({
       type: actionType.SET_CART_SHOW,
       cartShow: !cartShow,
     });
+  };
+  // update total
+  useEffect(() => {
+    let totalPrice = cartItems.reduce(function (accumulator, item) {
+      return accumulator + item.qty * item.price;
+    }, 0);
+    setTot(totalPrice);
+    console.log(tot);
+  }, [tot, flag]);
+  const clearCart = () => {
+    dispatch({
+      type: actionType.SET_CARTITEMS,
+      cartItems: [],
+    });
+    localStorage.setItem("cartItems", JSON.stringify([]));
   };
 
   return (
@@ -34,6 +51,7 @@ const CartContainer = () => {
           whileTap={{ scale: 0.75 }}
           className="flex items-center gap-2 p-1 px-2 my-2 bg-gray-100 rounded-md 
           hover:shadow-md cursor-pointer text-textColor text-base"
+          onClick={clearCart}
         >
           Clear <RiRefreshFill />
         </motion.p>
@@ -48,36 +66,14 @@ const CartContainer = () => {
           >
             {/* cart item */}
             {cartItems &&
+              cartItems.length > 0 &&
               cartItems.map((item) => (
-                <div
+                <CartItem
                   key={item.id}
-                  className="w-full p-1 px-2 rounded-lg bg-cartItem flex items-center gap-2"
-                >
-                  <img
-                    src={item?.imageUrl}
-                    alt=""
-                    className="w-20 h-20 max-w-[60px] rounded-full object-contain"
-                  />
-                  {/* name section */}
-                  <div className="flex flex-col gap2">
-                    <p className="text-base text-gray-50">{item?.title}</p>
-                    <p className="text-sm block text-gray-300 font-semibold">
-                      $ {item?.price}
-                    </p>
-                  </div>
-                  {/* button section */}
-                  <div className="group flex items-center gap-2 ml-auto cursor-pointer">
-                    <motion.div whileTap={{ scale: 0.75 }}>
-                      <BiMinus className="text-gray-50" />
-                    </motion.div>
-                    <p className="w-5 h-5 rounded-sm bg-cartBg text-gray-50 flex items-center justify-center">
-                      {item.qty}
-                    </p>
-                    <motion.div whileTap={{ scale: 0.75 }}>
-                      <BiPlus className="text-gray-50" />
-                    </motion.div>
-                  </div>
-                </div>
+                  item={item}
+                  setFlag={setFlag}
+                  flag={flag}
+                />
               ))}
           </div>
 
@@ -88,7 +84,7 @@ const CartContainer = () => {
           >
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-400 text-lg">Sub Total</p>
-              <p className="text-gray-400 text-lg">$ 8.5</p>
+              <p className="text-gray-400 text-lg">$ {tot}</p>
             </div>
 
             <div className="w-full flex items-center justify-between">
@@ -98,7 +94,9 @@ const CartContainer = () => {
             <div className="w-full border-b border-gray-600 my-2"></div>
             <div className="w-full flex items-center justify-between">
               <p className="text-gray-200 text-xl font-semibold">Total</p>
-              <p className="text-gray-200 text-xl font-semibold">$11.5</p>
+              <p className="text-gray-200 text-xl font-semibold">
+                $ {tot + 2.5}
+              </p>
             </div>
 
             {/* check out */}
